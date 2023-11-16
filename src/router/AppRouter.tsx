e78b0fbf01ba76f4870d7e8fcf18687911206983
Flips69 <aion.lipe@gmail.com>
2023-11-16 10:05:36 -0300

    Mexendo CSS e no Router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CadastroServico from "../components/servico/CadastroServico";
-import ListagemServico from "../components/servico/ListagemServico";
-import CadastroProfissional from "../components/profissionais/CadastroProfissional";
-import ListagemProfissional from "../components/profissionais/ListagemProfissional";
+import CadastroServico from "../components/Servicos/CadastroServico";
+import ListagemServico from "../components/Servicos/ListagemServico";
+import CadastroProfissional from "../components/Profissional/CadastroProfissional";
+import ListagemProfissional from "../components/Profissional/ListagemProfissional";
+import EditarServico from "../components/Servicos/EditarServico";
+import EditarProfissional from "../components/Profissional/EditarProfissional";
+import CadastroCliente from "../components/Clientes/CadastroCliente";
+import ListagemCliente from "../components/Clientes/ListagemCliente";
+import EditarCliente from "../components/Clientes/EditarCliente";
 
 const AppRouter = () => {
     return (
        <BrowserRouter>
          <Routes>
-           <Route path="cadastroServico" element={<CadastroServico />}></Route>
-           <Route path="listagemServico" element={<ListagemServico  />}></Route>
+           <Route path="cadastroServico" element={<CadastroServico />}/>
+           <Route path="listagemServico" element={<ListagemServico />}/>
+           <Route path="/servico/editar/:id"element={<EditarServico />}/>
 
-           <Route path="cadastroProfissional" element={<CadastroProfissional />}></Route>
-           <Route path="listagemProfissional" element={<ListagemProfissional  />}></Route>
+           <Route path="cadastroProfissional" element={<CadastroProfissional />}/>
+           <Route path="listagemProfissional" element={<ListagemProfissional />}/>
+           <Route path="/profissional/editar/:id"element={<EditarProfissional />}/>
+
+           <Route path="cadastroCliente" element={<CadastroCliente />}/>
+           <Route path="listagemCliente" element={<ListagemCliente />}/>
+           <Route path="/cliente/editar/:id"element={<EditarCliente />}/>
+
 
         </Routes>
        </BrowserRouter>
